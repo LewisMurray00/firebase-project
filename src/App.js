2ad43dd69299e2bb535749bbf253d8aea9993a6c
Lewis Murray <lewisdmurray00@gmail.com>
2021-10-17 20:37:40 +0100

    handleLogin function wrote

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
+/* eslint-disable default-case */
 import './App.css';
 import { useState, useEffect } from 'react';
+import fire from './fire';
 
 function App() {
 
@@ -13,7 +15,24 @@ function App() {
 
   const [hasAccount, setHasAccount] = useState(false);
 
-  
+  const handleLogin = () => {
+    fire
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .catch(err =>{
+        switch(err.code){
+          case "auth/invalid-email":
+          case "auth/user-disabled":
+          case "auth/user-not-found":
+            setEmailError(err.message);
+            break;
+          case "auth/wrong-password":
+            setPasswordError(err.message);
+            break;
+        }
+      })
+  }
+
 
   return (
     <div className="App">
