68b9c273953bfae48ed87de8005655faa6baf223
Lewis Murray <lewisdmurray00@gmail.com>
2021-10-17 20:28:10 +0100

    Added the states needed to add a log in system

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+
+  const [user, setUser] = useState('');
+  
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const [emailError, setEmailError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+
+  const [hasAccount, setHasAccount] = useState(false);
 
   
 
