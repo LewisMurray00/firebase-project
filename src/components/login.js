6efebb88cbea7d73a69a48d70da8d57c5c3438f8
Lewis Murray <lewisdmurray00@gmail.com>
2021-10-19 20:40:04 +0100

    Added props as an object in the login.js file

--- src/components/login.js
+++ src/components/login.js
@@ -1,6 +1,20 @@
 import React from 'react'
 
-const Login = () => {
+const Login = (props) => {
+
+    const {
+    email, 
+    setEmail,
+    password, 
+    setPassword, 
+    handleLogin,
+    handleSignup,
+    hasAccount,
+    setHasAccount,
+    emailError,
+    passwordError
+    } = props;
+
     return (
         <section className="login">
             
