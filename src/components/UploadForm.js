f3c7c04f1558f8a8249af3b61c4aa63c53c95bb2
Lewis Murray <lewisdmurray00@gmail.com>
2021-10-26 22:00:53 +0100

    Created an array containing strings of the allowed image types

--- src/components/UploadForm.js
+++ src/components/UploadForm.js
@@ -1,9 +1,29 @@
 import React from 'react'
+import { useState } from 'react';
 
 const UploadForm = () => {
+
+    //Creating an array of allowed image types
+    const imgTypes = [
+        'image/png',
+        'image/jpeg'
+    ];
+
+    const [file,setFile] = useState(null);
+
+    const changeHandler = (e)=>{
+        let selected = e.target.files[0];
+        //set the state if a file is selected
+        if(selected){
+            setFile(selected);
+            //file is stored in local state
+
+        }
+    }
+
     return (
         <form>
-            <input type="file" />
+            <input type="file" onChange={changeHandler} />
         </form>
     )
 }
