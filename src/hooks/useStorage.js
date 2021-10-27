d62eeecb9eb0a8e7075707dc729dba44a257c95a
Lewis Murray <lewisdmurray00@gmail.com>
2021-10-27 22:38:29 +0100

    finished the new hook created and exported it

--- src/hooks/useStorage.js
+++ src/hooks/useStorage.js
@@ -11,6 +11,25 @@ const useStorage =(file)=>{
     //use the storage sdk to upload the image to the database and load images 
     useEffect(()=>{
         //references to a file inside the firebase storage
-        const storageRef = projectStorage.ref(file.name)
+        const storageRef = projectStorage.ref(file.name);
+
+        storageRef.put(file).on('state_changed', (snap)=>{
+            //creates a snapshot in time of the upload
+            
+            //work out the total percentage of the upload
+            let percentage = (snap.bytesTransferred / snap.totalBytes) * 100;
+            setProgress(percentage);
+        }, (err)=>{
+            //Catch the error if there has been a problem uploading
+            setError(err)
+        }, async()=>{
+            //Check to see if the file has been successfully uploaded
+            const url = await storageRef.getDownloadURL();
+            setUrl(url);
+        })
     }, [file])
+
+    return {progress, url, error }
 }
+
+export default useStorage;
