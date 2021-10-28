8dab221fd14fabb6b3919712876e250d7f8ac826
Lewis Murray <lewisdmurray00@gmail.com>
2021-10-28 17:46:35 +0100

    Created the custom useEffect hook that snapshots the database at that moment of time grabbing the data and id and putting them into an array

--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -1,2 +1,24 @@
 import { useState, useEffect } from "react";
 import { projectFirestore } from "../fire";
+
+const useFirestore = (collection) =>{
+    const [docs,setDocs]=useState([]);
+
+    //creating a useEffect hook that will rerun everytime the collection changes
+    useEffect(()=>{
+        projectFirestore.collection(collection)
+        .orderBy('createdAt', 'desc')
+            .onSnapshot((snap)=>{
+                //takes a snapshot of that moment in time of the database
+                let documents = [];
+                //cycles through the documents at that moment in time
+                snap.forEach(doc=>{
+                    //gets us all the data and pushes it to the documents array
+                    documents.push({...doc.data(), id: doc.id})
+                })
+                setDocs(documents);
+            })
+    }, [collection])
+
+    return {docs};
+}
