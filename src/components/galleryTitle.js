a414b7de94286fce5208bcdd3895d3540b68b6bf
Lewis Murray <lewisdmurray00@gmail.com>
2021-11-01 20:15:09 +0000

    changed up the title

--- src/components/galleryTitle.js
+++ src/components/galleryTitle.js
@@ -4,7 +4,7 @@ const GalleryTitle = () => {
     return (
         <div className="gallery-title">
             <h1>Photo Gallery</h1>
-            <h2>Your Pictures</h2>
+            <h2>Live Pictures</h2>
         </div>
     )
 }
