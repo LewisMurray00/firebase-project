27fa659885de2c89ca32ced794c8dd662b51d5dc
Lewis Murray <lewisdmurray00@gmail.com>
2021-11-01 20:02:08 +0000

    Deploying to netlify

--- src/components/imageGrid.js
+++ src/components/imageGrid.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import useFirestore from '../hooks/useFirestore'
-import {motion} from 'framer-motion';
 
 
 const ImageGrid = ({setSelectedImg}) => {
@@ -10,10 +9,9 @@ const ImageGrid = ({setSelectedImg}) => {
     return (
         <div className="img-grid">
             {docs && docs.map(doc => (
-                <motion.div className="img-wrap" key={doc.id} onClick={()=>setSelectedImg(doc.url)}>
-                
+                <div className="img-wrap" key={doc.id} onClick={()=>setSelectedImg(doc.url)}>
                     <img src={doc.url} alt="Uploaded" />
-                </motion.div>
+                </div>
             ))}
         </div>
     )
